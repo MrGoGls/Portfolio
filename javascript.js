7e347b269bd8d4cda72614b3ba74e63f1e40069c
MrGoGls <57757077+MrGoGls@users.noreply.github.com>
2020-09-29 11:34:06 +0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+unction  changeCss() {
+            var w =  screen.width;
+            var h =  screen.height;
+            if (w == '1920')
+            {
+               document.getElementById("stylesheet").href="style.css";
+            }
+            else
+            {
+              if (w ==  '1366')
+                {
+              
